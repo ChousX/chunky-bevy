65d8526915caaf129a278a4c27144a239caa32fd
A. Garrett Gelwick III <gmanenergy@gmail.com>
2025-11-13 12:53:04 +0000

    added basic setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-use bevy::prelude::*;
+use std::collections::HashMap;
+use bevy::{
+    ecs::{
+        component::HookContext,
+        query::{QueryData, QueryFilter},
+        world::DeferredWorld,
+    },
+    prelude::*,
+};
 
 #[derive(Component)]
 #[require(ChunkPos, Visibility)]
@@ -43,19 +51,18 @@ fn on_add_chunk_pos(mut world: DeferredWorld, HookContext { entity, .. }: HookCo
 
 pub struct ChunkManager{
     chunk_size: Vec3,
+    chunks: HashMap<IVec3, Entity>
 }
 
 impl ChunkManager{
     //Inserts a new chunk into manager
     //Returns the previous chunk id if there was one
     pub fn insert(&mut self, pos: IVec3, id: Entity) -> Option<Entity> {
-        let IVec3 { x, y, z } = pos;
-        //the or_default seems wrong to me
-        self.0.entry(z).or_default().insert(ivec2(x, y), id)
+        self.chunks.insert(pos, id)
     }
+
     ///Remove Chunk from ChunkManager
     pub fn remove(&mut self, pos: IVec3) -> Option<Entity> {
-        let IVec3 { x, y, z } = pos;
-        self.0.get_mut(&z)?.remove(&ivec2(x, y))
+        self.chunks.remove(pos)
     }
 }
