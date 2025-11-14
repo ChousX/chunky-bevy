d307950523ba1ad83809b13d9b6cd1f4d4fbf7ee
A. Garrett Gelwick III <gmanenergy@gmail.com>
2025-11-14 13:05:49 +0000

    added rustfmt to the flake

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,29 @@
-use std::collections::HashMap;
 use bevy::{
-    ecs::{
-        component::HookContext,
-        query::{QueryData, QueryFilter},
-        world::DeferredWorld,
-    },
+    ecs::{lifecycle::HookContext, world::DeferredWorld},
     prelude::*,
 };
+use std::collections::HashMap;
+
+pub struct ChunkyPlugin {
+    chunk_size: Vec3,
+}
+impl Plugin for ChunkyPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(ChunkManager::new(self.chunk_size));
+    }
+}
+impl ChunkyPlugin {
+    pub const THREE_DIMETION: Self = Self {
+        chunk_size: vec3(10.0, 10.0, 10.0),
+    };
+    //ToDo: Make 2d usable
+    //pub const TWO_DIMENTION: Self = Self {chunk_size: vec3(10.0, 1.0, 10.0)};
+}
+impl Default for ChunkyPlugin {
+    fn default() -> Self {
+        Self::THREE_DIMETION
+    }
+}
 
 #[derive(Component)]
 #[require(ChunkPos, Visibility)]
@@ -18,7 +35,7 @@ use bevy::{
 pub struct Chunk;
 
 ///Adds Chunk to ChunkManager
-fn on_add_chunk(mut world: DeferredWorld, HookContext { entity, .. }: HookContext){
+fn on_add_chunk(mut world: DeferredWorld, HookContext { entity, .. }: HookContext) {
     let chunk_pos = world.get::<ChunkPos>(entity).unwrap().0;
     world
         .get_resource_mut::<ChunkManager>()
@@ -32,7 +49,7 @@ fn on_remove_chunk(mut world: DeferredWorld, HookContext { entity, .. }: HookCon
     world
         .get_resource_mut::<ChunkManager>()
         .unwrap()
-        .remove(chunk_pos);
+        .remove(&chunk_pos);
 }
 
 #[derive(Component, Default, Deref, DerefMut)]
@@ -45,16 +62,26 @@ pub struct ChunkPos(pub IVec3);
 
 ///Updates Transform to match ChunkPos
 fn on_add_chunk_pos(mut world: DeferredWorld, HookContext { entity, .. }: HookContext) {
-    let translation = world.get::<ChunkPos>(entity).unwrap().into_vec3();
+    let chunk_pos = world.get::<ChunkPos>(entity).unwrap();
+    let chunk_size = world.get_resource::<ChunkManager>().unwrap().chunk_size;
+    let translation = chunk_pos.as_vec3() * chunk_size;
     world.get_mut::<Transform>(entity).unwrap().translation = translation;
 }
 
-pub struct ChunkManager{
+#[derive(Resource, Default)]
+pub struct ChunkManager {
     chunk_size: Vec3,
-    chunks: HashMap<IVec3, Entity>
+    chunks: HashMap<IVec3, Entity>,
 }
 
-impl ChunkManager{
+impl ChunkManager {
+    ///New chunk manager
+    pub fn new(chunk_size: Vec3) -> Self {
+        Self {
+            chunk_size,
+            chunks: default(),
+        }
+    }
     //Inserts a new chunk into manager
     //Returns the previous chunk id if there was one
     pub fn insert(&mut self, pos: IVec3, id: Entity) -> Option<Entity> {
@@ -62,7 +89,7 @@ impl ChunkManager{
     }
 
     ///Remove Chunk from ChunkManager
-    pub fn remove(&mut self, pos: IVec3) -> Option<Entity> {
+    pub fn remove(&mut self, pos: &IVec3) -> Option<Entity> {
         self.chunks.remove(pos)
     }
 }
