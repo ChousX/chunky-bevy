917d2aad8cff6a6a2d19dd69345a00a4062167ac
A. Garrett Gelwick III <gmanenergy@gmail.com>
2025-11-14 14:03:12 +0000

    added more commands to ChunkManager

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,6 +3,7 @@ use chunky::*;
 
 fn main() {
     let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
     app.add_plugins(ChunkyPlugin::default());
     app.run();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,25 @@ use bevy::{
     prelude::*,
 };
 use std::collections::HashMap;
+pub mod prelude {
+    pub use crate::{Chunk, ChunkPos, ChunkyPlugin};
+}
 
 pub struct ChunkyPlugin {
     chunk_size: Vec3,
 }
+
 impl Plugin for ChunkyPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(ChunkManager::new(self.chunk_size));
+        app.insert_resource(ChunkManager::new(self.chunk_size))
+            .init_state::<ChunkBoundryVisualizer>()
+            .add_systems(
+                Update,
+                chunk_boundry_visualizer.run_if(in_state(ChunkBoundryVisualizer::On)),
+            );
     }
 }
+
 impl ChunkyPlugin {
     pub const THREE_DIMETION: Self = Self {
         chunk_size: vec3(10.0, 10.0, 10.0),
@@ -19,6 +29,7 @@ impl ChunkyPlugin {
     //ToDo: Make 2d usable
     //pub const TWO_DIMENTION: Self = Self {chunk_size: vec3(10.0, 1.0, 10.0)};
 }
+
 impl Default for ChunkyPlugin {
     fn default() -> Self {
         Self::THREE_DIMETION
@@ -82,8 +93,13 @@ impl ChunkManager {
             chunks: default(),
         }
     }
-    //Inserts a new chunk into manager
-    //Returns the previous chunk id if there was one
+
+    pub fn get_size(&self) -> Vec3 {
+        self.chunk_size
+    }
+
+    ///Inserts a new chunk into manager
+    ///Returns the previous chunk id if there was one
     pub fn insert(&mut self, pos: IVec3, id: Entity) -> Option<Entity> {
         self.chunks.insert(pos, id)
     }
@@ -91,5 +107,35 @@ impl ChunkManager {
     ///Remove Chunk from ChunkManager
     pub fn remove(&mut self, pos: &IVec3) -> Option<Entity> {
         self.chunks.remove(pos)
+    }
+
+    ///Converts cordinits into chunk position
+    pub fn get_chunk_pos(&self, pos: &Vec3) -> IVec3 {
+        (*pos / self.chunk_size).as_ivec3()
+    }
+
+    ///Gets chunk entity if it exists
+    pub fn get_chunk(&self, chunk_pos: &IVec3) -> Option<Entity> {
+        self.chunks.get(chunk_pos).copied()
+    }
+
+    ///Gets chunk entity form world cordinits if it exists
+    pub fn get_chunk_form_pos(&self, pos: &Vec3) -> Option<Entity> {
+        self.get_chunk(&self.get_chunk_pos(pos))
+    }
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum ChunkBoundryVisualizer {
+    #[default]
+    On,
+    Off,
+}
+
+///Shows all existing chunk boundreis
+fn chunk_boundry_visualizer(chunk_manager: Res<ChunkManager>, chunks: Query<&ChunkPos>) {
+    let size = chunk_manager.get_size();
+    for ChunkPos(chunk_pos) in chunks.iter() {
+        todo!()
     }
 }
