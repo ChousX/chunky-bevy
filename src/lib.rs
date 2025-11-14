d391e8afcb6febc234ccca3997e4f8c529d23f12
A. Garrett Gelwick III <gmanenergy@gmail.com>
2025-11-14 15:36:36 +0000

    added chunk loading and impl chunk visulsation

--- src/lib.rs
+++ src/lib.rs
@@ -123,6 +123,38 @@ impl ChunkManager {
     pub fn get_chunk_form_pos(&self, pos: &Vec3) -> Option<Entity> {
         self.get_chunk(&self.get_chunk_pos(pos))
     }
+
+    pub fn is_loaded(&self, chunk_pos: &IVec3) -> bool {
+        self.chunks.contains_key(chunk_pos)
+    }
+}
+
+/// ChunkLoader(0, 0, 0) would generate one chunk derctly under the entity.
+/// ChunkLoader(1, 1, 1,) would generate a 3x3x3 cube where the senter chunk is the one derctly
+/// under the entity.
+#[derive(Component, Default, Debug)]
+pub struct ChunkLoader(pub IVec3);
+
+///Load Chunks Around ChunkLoader
+fn chunk_loader(
+    chunks: Query<(&ChunkLoader, &GlobalTransform)>,
+    chunk_manager: Res<ChunkManager>,
+    mut commands: Commands,
+) {
+    for (ChunkLoader(loading_radius), g_transform) in chunks.iter() {
+        let translation = g_transform.translation();
+        let in_chunk = chunk_manager.get_chunk_pos(&translation);
+        for x in -loading_radius.x..=loading_radius.x {
+            for y in -loading_radius.y..=loading_radius.y {
+                for z in -loading_radius.z..=loading_radius.z {
+                    let target_chunk = in_chunk + ivec3(x, y, z);
+                    if !chunk_manager.is_loaded(&target_chunk) {
+                        commands.spawn((Chunk, ChunkPos(target_chunk)));
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
@@ -133,9 +165,23 @@ pub enum ChunkBoundryVisualizer {
 }
 
 ///Shows all existing chunk boundreis
-fn chunk_boundry_visualizer(chunk_manager: Res<ChunkManager>, chunks: Query<&ChunkPos>) {
-    let size = chunk_manager.get_size();
+fn chunk_boundry_visualizer(
+    chunk_manager: Res<ChunkManager>,
+    chunks: Query<&ChunkPos>,
+    mut gizmos: Gizmos,
+) {
+    let chunk_size = chunk_manager.get_size();
     for ChunkPos(chunk_pos) in chunks.iter() {
-        todo!()
+        let bottom_left = chunk_pos.as_vec3() * chunk_size;
+        let bottom_right = bottom_left + Vec3::new(chunk_size.x, 0.0, 0.0);
+        let top_right = bottom_right + Vec3::new(0.0, chunk_size.y, 0.0);
+        let top_left = bottom_left + Vec3::new(0.0, chunk_size.y, 0.0);
+
+        let color = bevy::color::palettes::tailwind::GREEN_500;
+
+        gizmos.line(top_left, top_right, color);
+        gizmos.line(top_right, bottom_right, color);
+        gizmos.line(bottom_right, bottom_left, color);
+        gizmos.line(bottom_left, top_left, color);
     }
 }
